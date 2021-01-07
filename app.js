46c517055e7296c6a17ba38fdd40bd0c0d4f9c2c
alexmeh96 <alex.meh96@mail.ru>
2021-01-08 02:20:35 +0300

    v-if

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ const App = {
       this.inputValue = event.target.value
     },
     addNewNote() {
-      this.notes.push(this.inputValue)
-      this.inputValue = ''
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    toUpperCase(note) {
+      return note.toUpperCase()
     },
     removeNote(idx, event) {
       console.log(event)
