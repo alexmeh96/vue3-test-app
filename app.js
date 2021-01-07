6b4c03f0d840e77b6e575bda50488a040a1d8e49
alexmeh96 <alex.meh96@mail.ru>
2021-01-08 02:03:12 +0300

    v-for

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const App = {
     return {
       title: 'Список заметок',
       placeholderString: 'Введите название заметки',
-      inputValue: ''
+      inputValue: '',
+      notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
     }
   },
   methods: {
     inputChangeHandler(event) {
       this.inputValue = event.target.value
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue)
+      this.inputValue = ''
+    },
+    removeNote(idx, event) {
+      console.log(event)
+      this.notes.splice(idx, 1)  //delete element
     }
   }
 }
